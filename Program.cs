56c138ac8a4c7fd610021201143f1fcaa854cb09
purfeld <wojo111115@wp.pl>
2020-03-27 22:05:41 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Relief
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
